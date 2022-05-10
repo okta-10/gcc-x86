c3f14559b984bcf9cbbb4cf298023b779a78a467
Oktapra Amtono <oktapra.amtono@gmail.com>
2022-05-10 12:26:53 +0700

    x86: Update build date to 20220510
    - GCC : x86_64-elf-gcc (Mystic GCC) 13.0.0 20220510 (experimental)
    
    - Binutils : GNU ld (Mystic binutils) 2.38.50.20220510
    
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

---
+++ lib/gcc/x86_64-elf/13.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=x86_64-elf --disable-decimal-float --disable-docs --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-threads --disable-tls --disable-werror --enable-checking=release --enable-default-ssp --enable-gold --enable-graphite=yes --enable-initfini-array --enable-languages=c,c++ --enable-ld=default --enable-lto --enable-plugin --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot --with-pkgversion='Mystic GCC' --prefix=/drone/src/build-gcc/output/gcc-x86";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
